6a1ad3d447e806e55aa4bb6c13f08b9301f12ae0
DaSoloX <jarbosa259@gmail.com>
2021-03-23 20:17:00 +0200

    CharUI_2
    + hunger, thirst, ammo max

--- Assets/Player/Scripts/PlayerUI.cs
+++ Assets/Player/Scripts/PlayerUI.cs
@@ -14,8 +14,6 @@ public class PlayerUI : MonoBehaviour
     void Start()
     {
         ps = gameObject.GetComponent<PlayerStats>();
-        textHunger = GameObject.Find("FirstPersonPlayer/Canvas/timer_bg/Hunger").GetComponent<Text>();
-        textThirst = GameObject.Find("FirstPersonPlayer/Canvas/timer_bg/Thirst").GetComponent<Text>();
         textAmmo = GameObject.Find("FirstPersonPlayer/Canvas/Ammo_bg/Ammo_count").GetComponent<Text>();
         ammoPanel = GameObject.Find("FirstPersonPlayer/Canvas/Ammo_bg");
         playerStats = GameObject.Find("FirstPersonPlayer/Canvas/status_bar");
@@ -24,28 +22,26 @@ public class PlayerUI : MonoBehaviour
     }
 
     // stat - текущее состояние хп/стамины | Для изменения статусбара
-    private void updateBars(string valType, float stat) {
+    private void updateBars(string contType, string valType, float stat) {
         for (int i = 5; i <= 100; i+=5) {
-            //Debug.Log(i);
-            if (i > stat) { playerStats.transform.Find(valType + "_bg/" + valType + "_container/" + valType + i.ToString()).gameObject.active=false; }
-            else { playerStats.transform.Find(valType + "_bg/" + valType + "_container/" + valType + i.ToString()).gameObject.active = true; }
+            if (i > stat) { playerStats.transform.Find(contType + "/" + valType + "_bg/" + valType + "_container/" + valType + i.ToString()).gameObject.active = false; }
+            else { playerStats.transform.Find(contType + "/" + valType + "_bg/" + valType + "_container/" + valType + i.ToString()).gameObject.active = true; }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        textHunger.text = Mathf.Round(ps.hunger).ToString();
-        textThirst.text = Mathf.Round(ps.thirst).ToString();
-
-        updateBars("hp", ps.health);
-        //updateBars("stamina", ps.stamina);
+        updateBars("hpAndStamina", "hp", ps.health);
+        //updateBars("hpAndStamina", "stamina", ps.stamina);
+        updateBars("hungerAndThirst", "hunger", ps.hunger);
+        updateBars("hungerAndThirst", "thirst", ps.thirst);
 
         //Debug.Log(gunContainer+" | "+gunContainer.transform.childCount);
         if (gunContainer.transform.childCount > 0)
         {
             if (!ammoPanel.active) { ammoPanel.active = true; }
-            textAmmo.text = gunContainer.transform.GetChild(0).gameObject.GetComponent<Projectile_weapon>().bulletsLeft.ToString();
+            textAmmo.text = gunContainer.transform.GetChild(0).gameObject.GetComponent<Projectile_weapon>().bulletsLeft.ToString() + "/" + gunContainer.transform.GetChild(0).gameObject.GetComponent<Projectile_weapon>().magazineSize.ToString();
         }
         else {
             ammoPanel.active = false;
