d3a77ea4ec1e4ff5a1bfb3cb62b804c7ed63da37
DaSoloX <jarbosa259@gmail.com>
2021-03-30 00:07:38 +0300

    MainMenu_v1

---
+++ Assets/ui/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void menuResume() {
+        Debug.Log("Resume clicked");
+    }
+    public void menuSave() {
+        Debug.Log("Save clicked");
+    }
+    public void menuLoad() {
+        Debug.Log("Load clicked");
+    }
+    public void menuSettings() {
+        Debug.Log("Settings clicked");
+    }
+    public void menuExit() {
+        Debug.Log("Exit clicked");
+    }
+}
