5edab6e71c64998ca57589b4a3bcc8fc282262e9
Marcensvire <63587144+Marcensvire@users.noreply.github.com>
2021-03-30 08:43:47 +0300

    Exit script

--- Assets/Player/Scripts/DeathTintScript.cs
+++ Assets/Player/Scripts/DeathTintScript.cs
@@ -6,24 +6,29 @@ using UnityEngine;
 public class DeathTintScript : MonoBehaviour
 {
     private PlayerControls controls;
+    private PlayerStats characterStats;
     // Start is called before the first frame update
     void Start()
     {
         controls = new PlayerControls();
         controls.Enable();
         controls.WorldActions.ReloadScene.performed += context => ReloadScene();
+        characterStats = GetComponent<PlayerStats>();
     }
 
     private void ReloadScene()
     {
-        // Scene scene = SceneManager.GetActiveScene();
-        //  SceneManager.LoadScene(scene.buildIndex+0);
-        Application.Quit();
+          Scene scene = SceneManager.GetActiveScene();
+          SceneManager.LoadScene(scene.buildIndex+0);
+        //Application.Quit();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (characterStats.isDead == true)
+        {
+            Volume.enabled = true;
+        }
     }
 }
